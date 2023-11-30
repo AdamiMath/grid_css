cb4e1acd69d7efd981b7f2c849ecb9b789963573
matheusadami.lima@gmail.com <matheusadami.lima@gmail.com>
2023-11-30 11:37:20 -0300

    tentativa de subir

--- index.js
+++ index.js
@@ -1,20 +1,18 @@
 const tabs = document.querySelectorAll('.js-tabmenu li');
 const tabcontent = document.querySelectorAll('.js-tabcontent');
-tabcontent[0].classList.add('ativo');
 const caracteristicas = document.querySelector('.caracteristicas');
 const divDentroDeCaracteristicas = caracteristicas.querySelector('div');
 
 function ativatab(index){
-    tabcontent.forEach((section, i) => {
+    tabcontent.forEach((section) => {
         section.classList.remove('ativo');
     });
 
     tabcontent[index].classList.add('ativo');
-
-    if(tabcontent[1].classList.contains('ativo')){
-        divDentroDeCaracteristicas.style.background = 'linear-gradient(135deg, #0000a1, #5f5c5c)';
-
-    }
+    tabcontent[0].classList.contains('ativo') ? divDentroDeCaracteristicas.style.background = 'red' : console.log('');
+    tabcontent[1].classList.contains('ativo') ? divDentroDeCaracteristicas.style.background = 'blue' : console.log('');
+    tabcontent[2].classList.contains('ativo') ? divDentroDeCaracteristicas.style.background = 'green' : console.log('');
+    tabcontent[3].classList.contains('ativo') ? divDentroDeCaracteristicas.style.background = 'yellow   ' : console.log('');
 
 
 }
@@ -27,11 +25,3 @@ tabs.forEach((itemmenu, index)=>{
 })
 
 
-
-
-function Pessoa(nome, altura){
-    this.nome = nome;
-    this.altura =  altura;
-}
-
-const matheus = new Pessoa('matheus', 1,74);
