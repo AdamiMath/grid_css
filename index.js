ba91cb069794b69a3b5d3a0a5528fd47a6fb4851
AdamiMath <matheusadami.lima@gmail.com>
2024-08-16 19:45:56 -0300

    trocando algumas coisas

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const tabs = document.querySelectorAll('.js-tabmenu li');
 const tabcontent = document.querySelectorAll('.js-tabcontent');
-<<<<<<< HEAD
 
 function ativatab(index) {
     tabcontent.forEach((section, i) => {
@@ -33,30 +32,3 @@ tabs.forEach((itemmenu, index) => {
         ativatab(index);
     });
 });
-=======
-const caracteristicas = document.querySelector('.caracteristicas');
-const divDentroDeCaracteristicas = caracteristicas.querySelector('div');
-
-function ativatab(index){
-    tabcontent.forEach((section) => {
-        section.classList.remove('ativo');
-    });
-
-    tabcontent[index].classList.add('ativo');
-    tabcontent[0].classList.contains('ativo') ? divDentroDeCaracteristicas.style.background = 'red' : console.log('');
-    tabcontent[1].classList.contains('ativo') ? divDentroDeCaracteristicas.style.background = 'blue' : console.log('');
-    tabcontent[2].classList.contains('ativo') ? divDentroDeCaracteristicas.style.background = 'green' : console.log('');
-    tabcontent[3].classList.contains('ativo') ? divDentroDeCaracteristicas.style.background = 'yellow   ' : console.log('');
-
-
-}
-
-tabs.forEach((itemmenu, index)=>{
-    itemmenu.addEventListener('click', (e) => {
-        e.preventDefault();
-        ativatab(index);
-    })
-})
-
-
->>>>>>> cb4e1acd69d7efd981b7f2c849ecb9b789963573
